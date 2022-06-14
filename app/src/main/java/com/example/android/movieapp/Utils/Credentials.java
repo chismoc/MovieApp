aa21668dfa43da5a1cb019d7c111e97e2769fd65
chismoc <chisomo.chingwalu@yahoo.co.uk>
2022-06-14 21:28:03 +0200

    dependancies added

---
+++ app/src/main/java/com/example/android/movieapp/Utils/Credentials.java
@@ -0,0 +1,10 @@
+package com.example.android.movieapp.Utils;
+
+public class Credentials {
+
+    //Base Url for request
+    public static final String BASE_URL = "https://api.themoviedb.org/";
+
+    public static final String API_KEY = "880843f1bf799de7677a489143f9226b";
+
+}
